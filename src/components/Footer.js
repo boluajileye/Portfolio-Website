d02dcec4931b33f808e66f368e81045434546bcc
boluajileye <boluwatifeajileye@gmail.com>
2022-06-12 21:14:36 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="footer">
     <a href="https://github.com/boluajileye" target="_blank"><i className="fa fa-github-square github" id="github"></i></a>
     <a href="https://www.linkedin.com/in/ajileye-boluwatife-168896198" target="_blank"><i className="fa fa-linkedin" id="linkedin"></i></a>
-    <a href="https://gitconnected.com/boluajileye" target="_blank"><i className="fa fa-file" id="gitconnected"></i></a>
+    <a href="https://gitconnected.com/boluajileye/resume" target="_blank"><i className="fa fa-file" id="gitconnected"></i></a>
   </div>
  </>
   );
--- src/data.js
+++ src/data.js
@@ -3,8 +3,8 @@ export const portfolio = [
       name: 'My Portfolio Website', 
       description: 'This is my first react project built up to show a little about myself, what i do, projects completed and a way to contact me ', 
       src: 'portfolio.png', 
-      repository: 'https://github.com/thiagodanobrega/recipes-app', 
-      site: 'https://recipes-app-thiagodanobrega.vercel.app/', 
+      repository: 'https://github.com/boluajileye/Portfolio-Website', 
+      site: 'https://boluajileye.netlify.app', 
       technologies: ['React', 'React-Router', 'Hooks', 'CSS3', 'HTML5', 'ES6', 'React-Icon-Cloud'],
     },
     
